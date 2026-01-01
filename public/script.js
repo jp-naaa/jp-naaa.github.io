8f5946b6ad92c323cfd389e8bda1ef0fd786d16c
Masao Takaku <tmasao@acm.org>
2026-01-01 13:29:10 +0900

    updated. #25 #19

--- public/script.js
+++ public/script.js
@@ -1,29 +1,51 @@
-
+function extract_triples(triples) {
+  const out = {};
+  for (const q of triples) {
+    if (q.object.termType !== "Literal") continue;
+    const s = q.subject.value;
+    const p = q.predicate.value;
+    const entry = {
+      value: q.object.value,
+      lang: q.object.language || null,               // 言語なしは null
+      datatype: q.object.datatype?.value || null
+    };
+    out[s] ??= {};
+    out[s][p] ??= [];
+    out[s][p].push(entry);
+  }
+  return out;
+}
 
 let fetcher = new window.ldfetch();
 function fetch_jp_cos(url, elem) {
   let main = async function () {
-    let objects = await fetcher.get(url).then(response => {
-      return fetcher.frame(response.triples, {'@graph':{}});
-    });
+    const objects = await fetcher.get(url).then(response =>
+      extract_triples(response.triples)
+    );
+    const uri = decodeURI(url);
+    console.log(uri);
     console.log(objects);
-    objects["@graph"].forEach(e => {
-      if (e["@id"] == url) {
-        let parent = elem.parentNode;
-        let cscode = e["http://purl.org/dc/terms/identifier"]["@value"];
-        let cos = e["https://w3id.org/jp-cos/courseOfStudy"]["@id"];
-        parent.innerHTML += ` <span class="sectionNumberHierarchy">${e["https://w3id.org/jp-cos/sectionNumberHierarchy"]["@value"]}</span>`;
-        parent.innerHTML += `<br><span class="sectionText">${e["https://w3id.org/jp-cos/sectionText"]["@value"]}</span>`;
-        //console.log(parent);
-    }
-    });
-  }
-  try {
+    if (objects[uri]) {
+      e = objects[uri];
+      const parent = elem.parentNode;
+      if (e["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"] == "https://w3id.org/jp-cos/Item") {
+        parent.innerHTML += ` <span class="sectionNumberHierarchy">${e["https://w3id.org/jp-cos/sectionNumberHierarchy"][0].value}</span>`;
+        parent.innerHTML += `<br><span class="sectionText">${e["https://w3id.org/jp-cos/sectionText"][0].value}</span>`;
+      } else {
+        for (const literal of e["http://schema.org/name"]) {
+          if (literal.lang == "ja") {
+            parent.innerHTML += ` <span class="jp-cos name">${literal.value}</span>`;
+          }
+        };
+      }
+    };
+  };
+    try {
     main();
   } catch (e) {
     console.error(e);
   }
-}
+};
 $("dl.row dd a").each(function(link){
   //let url = "https://w3id.org/jp-cos/8220233111000000";
   let url = this.href.toString();
